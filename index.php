923255aa269bb8a057e20cfe2d535a622e5c61ac
TAISEI0704 <132648502+TAISEI0704@users.noreply.github.com>
2023-05-20 23:15:56 +0800

    practice2-9

--- index.php
+++ index.php
@@ -93,4 +93,60 @@ switch($x){
         break;
 }
 
+$month = 7;
+switch($month){
+    case 1:
+        echo "January<br>";
+        break;
+
+    case 2:
+        echo "February<br>";
+        break;
+
+    case 3:
+        echo "March<br>";
+        break;
+
+    case 4:
+        echo "April<br>";
+        break;
+
+    case 5:
+        echo "May<br>";
+        break;
+
+    case 6:
+        echo "June<br>";
+        break;
+
+    case 7:
+        echo "July<br>";
+        break;
+
+    case 8:
+        echo "August<br>";
+        break;
+
+    case 9:
+        echo "September<br>";
+        break;
+
+    case 10:
+        echo "October<br>";
+        break;
+
+    case 11:
+        echo "November<br>";
+        break;
+
+    case 12:
+        echo "December<br>";
+        break;
+    
+    default:
+        echo "月が存在しません";
+        break;
+
+}
+
 ?>
